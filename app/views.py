d3a3a9ea6562cd4b5ec5e61d07dc0d8912b8042b
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-22 17:00:03 +0300

    RENDERING WELCOME PAGE

--- app/views.py
+++ app/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 
 # Create your views here.
+# @login_required(login_url='/accounts/login/')
+
 def welcome(request):
-    return render(request, 'welcome.html') 
+    return render(request, 'welcome.html')
