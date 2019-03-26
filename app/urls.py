00ad89fb1a64cf73875f7f85da4fbfc52df3b57a
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-26 19:19:02 +0300

    adding the urls routes

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url('home', views.welcome, name = 'welcome'),
-    url(r'^$',views.signup, name='sign')
+    url(r'^$',views.signup, name='sign'),
+    url(r'^profile/$',views.profile, name='profile'),
+    url(r'^search/$',views.search_results, name='search'),
 ]
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
