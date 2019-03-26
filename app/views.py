fe1994d3c4600ed8db61c708b432ceacfb37cf5f
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-26 19:19:32 +0300

    creating the searcgh and profile methods

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect
 from .models import *
+from .forms import *
 
 
 # Create your views here.
@@ -15,6 +16,34 @@ def welcome(request):
 def signup(request):
     return redirect('/accounts/login/')
     return render(request, 'registration_form.html')
+
+def profile(request):
+    if request.method == 'POST':
+        form = UploadForm(request.POST,request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form =UploadForm()
+
+        profile = Profile.objects.all()
+        all_profile = Profile.objects.all()
+    return render(request,'profile.html', locals())
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Project.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"searched_articles": searched_articles})
+
+    else:
+        message = "please enter a name"
+        return render(request, 'search.html',{"message":message})
+
 
 # def signup(request):
 #     return redirect('/accounts/login/')
