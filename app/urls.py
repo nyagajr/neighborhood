55a04f8609fb99e8f6269d2abafcc1436dc6d180
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-26 15:30:45 +0300

    setting the urls for login

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$', views.welcome, name = 'welcome'),
+    url('home', views.welcome, name = 'welcome'),
+    url(r'^$',views.signup, name='sign')
 ]
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
