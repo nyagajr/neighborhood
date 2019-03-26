98955d9c0df46b15d39e861a6eb22655990d7769
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-26 20:33:18 +0300

    creating tests and redefining the urls

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import *
+
+
 
 # Create your tests here.
+class HoodTestClass(TestCase):
+    def setUp(self):
+        self.junior=Hood(hood_name='hooh',hood_photo = 'junior.jpg', location = 'usa',link_hood = 'mee', occupant_count = '100')
+
+    #testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.junior,Hood))
