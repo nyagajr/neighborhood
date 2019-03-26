42e9fb32fce9c57b20cdb8765d4ac7023576e2ec
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-26 15:31:07 +0300

    sign up class

--- app/views.py
+++ app/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,redirect
+from .models import *
+
 
 # Create your views here.
 # @login_required(login_url='/accounts/login/')
 
 def welcome(request):
     return render(request, 'welcome.html')
+
+def signup(request):
+    return redirect('/accounts/login/')
+    return render(request, 'registration_form.html')
 
 # def signup(request):
 #     return redirect('/accounts/login/')
