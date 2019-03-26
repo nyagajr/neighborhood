f449cd86b8dcd237e6c09e5af9a90dea52eace70
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-26 17:36:15 +0300

    adding tables in the model

--- app/models.py
+++ app/models.py
@@ -6,14 +6,18 @@ from django.db.models.signals import post_save
 # Create your models here.
 class Hood(models.Model):
     hood_name = models.CharField(max_length =30)
-    hood_photo = models.CharField(max_length = 30)
+    hood_photo = models.ImageField(upload_to = 'articles/')
     location = models.CharField(max_length = 30)
+    link_hood = models.CharField(max_length = 200)
     occupant_count = models.CharField(max_length =50)
+
+
     def __str__(self):
         return self.hood_name
 
 class Profile(models.Model):
     profile_name = models.CharField(max_length = 30)
+    dpic = models.ImageField(upload_to = 'articles/')
     email_address = models.CharField(max_length = 30)
     neighborhood_id = models.ForeignKey(Hood)
 
